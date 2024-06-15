1d2ff77d6b252613af5b22f232cfbc584dfcac3f
Tirths101 soni <tirthsoni56@gmail.com>
2024-06-15 15:27:22 +0000

    Add the basic layout of the gameboard

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random # import random numbers
+
+print("Welcome to Connect-IV")
+print("_____________________")
+
+potentialNumbers = ["I","II","III","IV","V","VI","VII"]
+gameBoard = [["","","","","","",""], ["","","","","","",""], ["","","","","","",""],
+["","","","","","",""], ["","","","","","",""], ["","","","","","",""]]
+
+rows = 6
+cols = 7
+
+def printGameBoard():
+    print("\n      I   II   III  IV    V   VI  VII  ", end="")
+    for x in range(rows):
+        print("\n   ------------------------------------")
+        print(x, " |", end="")
+        for y in range(cols):
+            if(gameBoard[x][y] == "🏎️"):
+                print("", gameBoard[x][y], end=" |")
+            elif(gameBoard[x][y] == "🏍️"):
+                print("", gameBoard[x][y], end=" |")
+            else:
+                print(" ", gameBoard[x][y], end="  |")
+    print("\n   ------------------------------------")
+
+printGameBoard()
